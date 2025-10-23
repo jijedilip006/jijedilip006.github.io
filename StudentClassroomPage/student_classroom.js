2fb24235cbdb98ac3b87e28321de62cc9d3b6788
Jijendran1010 <jijedilip007@gmail.com>
2025-10-23 08:49:44 +0800

    Update student_classroom.js

--- StudentClassroomPage/student_classroom.js
+++ StudentClassroomPage/student_classroom.js
@@ -47,7 +47,7 @@ async function loadClassrooms() {
   const { data: existing, error: existingError } = await supabase
     .from("classrooms")
     .select('classroom_id,"Draft Mode",Archived,students')
-    .cs("students", [user.email]);
+    .filter("students",'cs', [user.email]);
 
   if (existingError) {
     console.error("Error checking enrollment:", existingError);
